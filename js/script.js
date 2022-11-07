9b5d5b35460d40f9514de903d7296b85b51ec017
Angelo31294 <luongoangelo1@gmail.com>
2022-11-07 14:51:35 +0100

    Agg. array di oggetti - MILESTONE 0

--- js/script.js
+++ js/script.js
@@ -1,1 +1,42 @@
 "use strict"
+// 1. Creare l’array di oggetti con le informazioni fornite.
+
+const team = [
+    {
+        fullname: 'Wayne Barnett',
+        job: 'Founder & CEO',
+        image: 'wayne-barnett-founder-ceo.jpg'
+    },
+
+    {
+        fullname: 'Angela Caroll',
+        job: 'Chief Editor',
+        image: 'angela-caroll-chief-editor.jpg'
+    },
+
+    {
+        fullname: 'Walter Gordon',
+        job: 'Office Manager',
+        image: 'walter-gordon-office-manager.jpg'
+    },
+
+    {
+        fullname: 'Angela Lopez',
+        job: 'Social Media Manager',
+        image: 'angela-lopez-social-media-manager.jpg'
+    },
+
+    {
+        fullname: 'Scott Estrada',
+        job: 'Developer',
+        image: 'scott-estrada-developer.jpg'
+    },
+
+    {
+        fullname: 'Barbara Ramos',
+        job: 'Graphic Designer',
+        image: 'barbara-ramos-graphic-designer.jpg'
+    }
+];
+
+console.log(team);
